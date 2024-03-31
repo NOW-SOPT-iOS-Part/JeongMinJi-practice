45c211b0d1fda4b848efc12d15cea4e61d2e0709
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-04-01 01:56:24 +0900

    [Add] Pretendard 폰트 추가

--- 34th-Sopt-Seminar/WelcomeViewController.swift
+++ 34th-Sopt-Seminar/WelcomeViewController.swift
@@ -44,7 +44,7 @@ final class WelcomeViewController: UIViewController {
         let button = UIButton(frame: CGRect(x: 20, y: 498, width: 332, height: 58))
         button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
         button.setTitle("다시 로그인", for: .normal)
-        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
         button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
         button.addTarget(self, action: #selector(reLoginButtonDidTap), for: .touchUpInside)
         button.layer.cornerRadius = 6
