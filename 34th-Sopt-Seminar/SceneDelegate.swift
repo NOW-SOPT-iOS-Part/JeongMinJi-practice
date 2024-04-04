62745187f898c1373ceb1da710e8bc83d6a20b1d
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-04-04 22:49:57 +0900

    1주차 과제

--- 34th-Sopt-Seminar/SceneDelegate.swift
+++ 34th-Sopt-Seminar/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
          
-        let mainViewController = ViewController()
+        let mainViewController = LoginViewController()
         let navigationController = UINavigationController(rootViewController: mainViewController)
                 
         window?.rootViewController = navigationController
